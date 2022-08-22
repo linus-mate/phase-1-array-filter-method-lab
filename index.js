7a381d3b050b2bda58384d81d6e818b8d8982571
linus mutugi mate <mlynux@gmail.com>
2022-08-22 17:02:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, string) {
+    return drivers.filter(X => {
+        return X.toLowerCase() === string.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(x => {
+        return(
+           x.startsWith(string)  && 
+           x.charAt(x.length-1) !== string &&
+           !x.substring(1, x.length-1).includes(string)
+        );
+    });
+}
+
+function matchName(drivers, string){
+    return drivers.filter (X => {
+        return X.name === string;
+    });
+}
+
+//=> {
+  //  return drivers.filter (x => {
+       // return x.toLowerCase() === string.toLowerCase();
+ //   });
+//}
